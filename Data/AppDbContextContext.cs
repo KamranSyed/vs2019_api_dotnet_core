441365802dc2185d13123659ec051c2d99d0aa32
Kamran Syed <lovelykamran@gmail.com>
2019-07-10 13:42:47 +0500

    First Commit

---
+++ Data/AppDbContextContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestfulAPInetCore.Models
+{
+    public class AppDbContextContext : DbContext
+    {
+        public AppDbContextContext (DbContextOptions<AppDbContextContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RestfulAPInetCore.Models.Game> Game { get; set; }
+    }
+}
---
+++ Pages/Games/Details.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RestfulAPInetCore.Models;
+
+namespace RestfulAPInetCore.Pages.Games
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly RestfulAPInetCore.Models.AppDbContextContext _context;
+
+        public DetailsModel(RestfulAPInetCore.Models.AppDbContextContext context)
+        {
+            _context = context;
+        }
+
+        public Game Game { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Game = await _context.Game.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Game == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
